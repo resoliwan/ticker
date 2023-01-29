861629b8876232c6ee023466cb85d8e340d66bf3
resoliwan <resoliwan@naver.com>
2023-01-29 17:54:17 +0900

    refactor add duplicate error handle

--- ticker/service_layer/handlers.py
+++ ticker/service_layer/handlers.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Callable, Dict, List, Type
 
 import pymysql
+import sqlalchemy
 from ticker.adapters.vendor_proxy import AbstractVendorProxy
 from ticker.domain import commands, events, model
 
@@ -18,9 +19,16 @@ def create_daily_prices(
 ):
     df = vendor_proxy.get_daily_prices(cmd.symbol, cmd.interval, cmd.a_range)
     with uow:
-        for _, row in df.iterrows():
-            uow.price.add(item=model.DailyPrice.create_from_price(row))
-        uow.commit()
+        try:
+            for _, row in df.iterrows():
+                uow.price.add(item=model.DailyPrice.create_from_price(row))
+            uow.commit()
+        except (sqlalchemy.exc.IntegrityError, pymysql.err.IntegrityError) as e:
+            if e.orig.args[0] == 1062:
+                # ignore duplicate key error
+                pass
+            else:
+                raise e
 
 
 EVENT_HANDLERS = {}
